4f37f95d3b38a3c7af0af4f4f73f12bff9e0e478
mckimssongg <soymckimsson286@gmail.com>
2022-10-07 18:40:42 -0600

    sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'empleados',
+    component: ListaEmpleadosComponent,
+  },
+  {
+    // Nos redirige a la ruta empleados si no se encuentra la ruta especificada en la url
+    path: '',
+    redirectTo: '/empleados',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 @NgModule({
-  declarations: [AppComponent, ListaEmpleadosComponent],
+  declarations: [AppComponent, ListaEmpleadosComponent, SidebarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
